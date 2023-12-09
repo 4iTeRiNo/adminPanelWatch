d8ab939a1e9b5990d204b5b6b7c372723cddad59
4iTeRiNo <zubkovigor@mail.ru>
2023-12-09 23:36:17 +0200

    deploy app

---
+++ static/js/19.82ebff1c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[19],{514:function(s,e,i){"use strict";i.r(e);var t=i(2);i(0);e.default=function(){return Object(t.jsx)("div",{children:"\u0412\u0441\u0435 \u0440\u0430\u0431\u043e\u0442\u0430\u0435\u0442 \u043f\u043e\u043b\u0435\u0442\u0435\u043b\u0438 \u0437\u043d\u0430\u043a\u043e\u043c\u0438\u0442\u044c\u0441\u044f!"})}}}]);
+//# sourceMappingURL=19.82ebff1c.chunk.js.map
